29a19e6db98ebfb99ad09851af94819ec7cdee03
Jessica Alin Gonzalez <jessicaalin@Jessicas-MacBook-Pro.local>
2017-12-15 11:27:42 -0500

    fixed timer

--- src/app/doc-edit/doc-edit.component.ts
+++ src/app/doc-edit/doc-edit.component.ts
@@ -3,14 +3,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { DocApiService, Doc } from '../services/doc-api.service';
 
-export enum KEY_CODE {
-  RIGHT_ARROW = 39,
-  LEFT_ARROW = 37,
+export enum key_code {
   a = 65
 }
 
 var docText = null;
 var focusText = null;
+
+// OR WHY DOESN'T THIS WORK???
+// var inputValue = (<HTMLInputElement>document.getElementById('textInput')).value;
+// console.log(inputValue);
 
 @Component({
   selector: 'app-doc-edit',
@@ -20,11 +22,7 @@ var focusText = null;
 
 export class DocEditComponent implements OnInit {
 
-
   docInfo = new Doc()
-
-
-
 
   constructor(
     private activatedThing: ActivatedRoute,
@@ -39,6 +37,7 @@ export class DocEditComponent implements OnInit {
     })
   }
 
+
   startAjaxCall(id) {
     this.docThing.getOneDoc(id)
       .then((docResults: Doc) => {
@@ -52,8 +51,6 @@ export class DocEditComponent implements OnInit {
       })
   }
 
-
-
   submitEdit() {
     this.docThing.editOneDocText(this.docInfo._id, this.docInfo)
     .then(() => {
@@ -65,54 +62,45 @@ export class DocEditComponent implements OnInit {
     });
   }
 
-  value = 0;
-
   @HostListener('window:keyup', ['$event'])
   keyEvent(event: KeyboardEvent) {
     console.log(event);
-    console.log(this.value);
 
-    if (event.keyCode === KEY_CODE.RIGHT_ARROW) {
-      this.increment();
+    // if (event.keyCode === key_code.a) {
       this.timer();
-    }
-
-    if (event.keyCode === KEY_CODE.LEFT_ARROW) {
-      this.decrement();
-    }
-
-    if (event.keyCode === KEY_CODE.a) {
-      this.timer();
-    }
-  }
-
-  increment() {
-    this.value++;
-  }
-
-  decrement() {
-    this.value--;
+    // }
   }
 
   changeInterval = null;
   timer() {
     console.log("typing...");
     clearInterval(this.changeInterval)
-    this.changeInterval = setInterval(function(){
+    this.changeInterval = setInterval(() => {
       console.log("stopped typing.");
-      console.log(docText.text); // prints what is saved
-      console.log(docText.text.innerHtml);  // WORKS!
-      focusText = docText.text.innerHtml;
-
-
+      console.log(docText.text); // Works!
+      focusText = docText.text;
+      // remove(focusText);
+      console.log(focusText);
       // console.log(this.docThing.text); can't read of undefined
       // console.log(this.docInfo.text); // can't read text of undefined
       // console.log(this.docInfo); // undefined
+      // console.log(docText.text.innerHtml);  // undefined
 
+      focusText = focusText.slice(0, -1);
+      console.log(focusText);
+      // WILL THIS WORK???
+      this.docInfo.text = focusText;
 
-      clearInterval(this.changeInterval)
-    }, 1000);
+    }, 5000);
   }
 
+}
 
+// broken function - stops halfway through string
+function remove(string){
+  for(let i=0; i < string.length ; i++)
+  {
+  string = string.slice(0,-1);
+  console.log(string);
+  }
 }
